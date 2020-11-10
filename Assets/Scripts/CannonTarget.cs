f62d9ed62515ce59762608ad8f92692cbd3b6fa8
Michael Monaghan <michael@monaghan.co.uk>
2020-11-10 16:55:03 +0000

    Added target that removes/opens door and updated cannon with UI and proper controls

---
+++ Assets/Scripts/CannonTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonTarget : MonoBehaviour {
+    [SerializeField]
+    private GameObject m_linkedDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // Debug.Log("Trigger Entered");
+        if (other.gameObject.CompareTag("CannonBall")) {
+            //do stuff
+            m_linkedDoor.GetComponent<DoorController>().Open();
+        }
+    }
+}
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private bool m_isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_isOpen) {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    
+    
+    public void Open() {
+        m_isOpen = true;
+    }
+}
